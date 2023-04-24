8adcabaa51f3c8d60a0d2301639514a98fb1dc8e
TeftForK <131676859+TeftForK@users.noreply.github.com>
2023-04-24 12:19:14 -0700

    Update InvalidNumberException.java

--- InvalidNumberException.java
+++ InvalidNumberException.java
@@ -41,4 +41,11 @@ public class InvalidNumberException extends Exception {
         }
         return numbers;
     }
+        public int sumArray(int[] array) {
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            sum += array[i];
+        }
+        return sum;
+    }
 }
